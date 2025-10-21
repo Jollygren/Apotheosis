14b1a06689b9dc8b0c6139703b855aa8f06b3b67
Jollygren <evanwjolliffe@gmail.com>
2025-10-21 16:46:13 -0400

    Played with movement, added jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,44 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _speed = 30f;
+    [SerializeField] private float _jumpForce = 250f;
+    private bool _isJumping = false;
     private InputAction _moveAction;
+    private InputAction _jumpAction;
+    private CircleCollider2D _collider;
+    private Rigidbody2D _rigidbody;
     void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        _collider = GetComponent<CircleCollider2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = _moveAction.ReadValue<Vector2>();
-        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y) * _speed * Time.deltaTime;
-        transform.Translate(movement, Space.World);
+        Vector2 inputVector = _moveAction.ReadValue<Vector2>() * _speed * Time.deltaTime;
+        if (_jumpAction.IsPressed())
+        {
+            if (!_isJumping)
+            {
+                inputVector.y += _jumpForce * Time.deltaTime; // Add jump force
+                _isJumping = true;
+            }
+        }
+        
+        _rigidbody.AddForce(inputVector, ForceMode2D.Impulse);
+        if(_collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            _isJumping = false; // Reset jumping state when on the ground
+        }
     }
 }
