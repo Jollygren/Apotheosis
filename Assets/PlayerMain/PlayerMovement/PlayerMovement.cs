d7468a2d7bf9719cb58c7febf72c658df2140db1
S3thN3cr0n <ethanglennjohnson@gmail.com>
2025-10-22 01:55:43 -0400

    Added Floor Organization and Sprites for Scenes
    I added folders for the scenes for the different floors, they are currently empty. I worked on my testing area and got a decent movement system working, it sticks to the floor when trying to jump from a dead stop so I'm still working on it. Added some free sprite assets from the asset store to use as placeholders for our scenes if we can't find better ones.

--- Assets/PlayerMain/PlayerMovement/PlayerMovement.cs
+++ Assets/PlayerMain/PlayerMovement/PlayerMovement.cs
@@ -2,19 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour{
+public class PlayerMovement : MonoBehaviour {
 
+    public float groundSpeed;
+    public float jumpSpeed;
+    [Range(0, 1f)]
+    public float groundDecay;
     public Rigidbody2D body;
+    public BoxCollider2D groundCheck;
+    public LayerMask groundMask;
+    public bool grounded;
+    float xInput;
+    float yInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        body = GetComponent<Rigidbody2D>();
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        GetInput();
+        MoveWithInput();
     }
+
+    void FixedUpdate() {
+        CheckGround();
+        ApplyFriction();
+    }
+
+    void GetInput() {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+
+    void MoveWithInput() {
+        if (Mathf.Abs(xInput) > 0) {
+            body.linearVelocity = new Vector2(xInput * groundSpeed, body.linearVelocity.y);
+
+            float direction = Mathf.Sign(xInput);
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
+
+        if (Mathf.Abs(yInput) > 0 && grounded) {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * jumpSpeed);
+        }
+    }
+
+    void CheckGround() {
+        grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+    }
+
+    void ApplyFriction() {
+        if (grounded && xInput == 0 && yInput == 0) {
+            body.linearVelocity *= groundDecay;
+        }
+    }
+
 }
